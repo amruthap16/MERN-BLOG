5d882d3ee3407586758a67db934d5552502bfe87
amruthap16 <jaganccf.123@gmail.com>
2024-05-27 01:52:14 +0530

    add functionality to the sign up page

--- api/index.js
+++ api/index.js
@@ -22,13 +22,13 @@ app.use(express.json());
     app.use('/api/user', userRoutes);
     app.use('/api/auth',authRoutes);
 
-    app.use((err,req,res,next) => {
-        const statusCode = err.statusCode || 500;
-        const message = err.message || 'Internal Server Error';
-        res.status(statusCode).json({
-            successs: false,
-            statusCode,
-            message
-        });
-    });
+     app.use((err,req,res,next) => {
+         const statusCode = err.statusCode || 500;
+         const message = err.message || 'Internal Server Error';
+         res.status(statusCode).json({
+             successs: false,
+             statusCode,
+             message
+         });
+     });
        
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,10 @@
+// import React from 'react'
+
+// export default function SignUp() {
+//   return (
+//     <div>SignUp</div>
+//   )
+// }
 import React, { useState } from "react";
 import { Link,useNavigate } from "react-router-dom";
 import { Label, TextInput,Button, Alert, Spinner } from "flowbite-react";
@@ -42,20 +49,20 @@ const handleSubmit=async(e)=>{
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        
+        /* left/
         <div className="flex-1">
           <Link
             to="/"
             className=" font-bold dark:text-white  text-4xl"
           >
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Amrutha's
+              Usha's
             </span>
             Blog
           </Link>
           <p className="text-sm mt-5">This is demo project.You can sign up with your email and password or with Google.</p>
         </div>
-       
+        {/right/}
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
@@ -114,48 +121,3 @@ const handleSubmit=async(e)=>{
     </div>
   );
 }
-// import { Label, TextInput } from 'flowbite-react';
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-
-// export default function SignUp() {
-//   return (
-//     <div className=' mt-20'>
-//       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-6'>
-//         <div classNam='flex-1'>
-//         <Link to="/" className=' font-bold dark:text-white text-4xl'>
-//             <span class='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Amrutha's</span>
-//             Blog
-//         </Link>
-//         <p className='text-sm mt-5'>
-//           this is demo project. you can sign up with ur email and password or with google.
-//         </p>
-//         </div>
-
-
-
-//         <div className='flex-1'>
-//           <form className='flex flex-col gap-4'>
-//             <div>
-//               <Label value ='Your username' />
-//               <TextInput type='text' placeholder='Username' id='username'/>
-//             </div>
-//             <div>
-//               <Label value ='Your email' />
-//               <TextInput type='text' placeholder='Email' id='email'/>
-//             </div>
-//             <div>
-//               <Label value ='Your password' />
-//               <TextInput type='text' placeholder='Password' id='password'/>
-//             </div>
-//             <Button gradientDuoTone='purpleToPick' >
-//               Sign Up
-//             </Button>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-
